9f4187ce8d2287a078f8151e67d9708bda66e89f
Luke Hatcher <lukehatcher98@gmail.com>
2020-12-21 13:58:30 -0800

    covered number[] and arrays of objs

--- src/insertion.js
+++ src/insertion.js
@@ -1,14 +1,32 @@
-const insertion = (arr) => {
-  // if its an array of integers:
-  // neeed to check if string
-  for (let i = 0; i < arr.length; i++) {
-    const toBeInserted = arr[i];
-    let j = i - 1;
-    while (j >= 0 || j < arr[j]) {
-      arr[j + 1] = arr[j];
-      j -= 1;
+const insertion = (arr, key) => {
+  if (key) {
+    for (let i = 0; i < arr.length; i++) {
+      if (!arr[i][key] || typeof arr[i][key] !== 'number') {
+        console.error('must have all numeric elements and key must exist on all items');
+        return;
+      }
+      const toBeInserted = arr[i][key];
+      let j = i - 1;
+      while (j >= 0 && arr[j][key] > toBeInserted) {
+        arr[j + 1][key] = arr[j][key];
+        j -= 1;
+      }
+      arr[j + 1][key] = toBeInserted;
     }
-    arr[j + 1] = toBeInserted;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== 'number') {
+        console.error('must have all numeric elements');
+        return;
+      }
+      const toBeInserted = arr[i];
+      let j = i - 1;
+      while (j >= 0 && arr[j] > toBeInserted) {
+        arr[j + 1] = arr[j];
+        j -= 1;
+      }
+      arr[j + 1] = toBeInserted;
+    }
   }
   return arr;
 };

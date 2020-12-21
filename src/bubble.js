bc983c46ebe7c3d552c4cc03547d02e138eb2581
Luke Hatcher <lukehatcher98@gmail.com>
2020-12-20 22:03:41 -0800

    added handlers for array of arrays and updated tests

--- src/bubble.js
+++ src/bubble.js
@@ -1,23 +1,28 @@
-// export default function bubble(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr.length; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         const temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
-//       }
-//     }
-//   }
-//   return arr;
-// }
-
-const bubble = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+const bubble = (arr, key) => {
+  // if key is provided, the array is an array of objects
+  if (key) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        // check that each item has the desired key
+        if (!arr[j][key] || typeof arr[j][key] !== 'number') {
+          console.error('each item in the array must contain the provided key and its value must be a number');
+          return;
+        }
+        if (arr[j] > arr[j + 1]) {
+          const temp = arr[j + 1];
+          arr[j + 1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+          const temp = arr[j + 1];
+          arr[j + 1] = arr[j];
+          arr[j] = temp;
+        }
       }
     }
   }
--- test/bubble.test.js
+++ test/bubble.test.js
@@ -1,6 +1,6 @@
 const algoSorter = require('../src/index');
 
-describe('bubble sort', () => {
+describe('Bubble sort', () => {
   it('should numerically order an unordered array', () => {
     const exampleArr = [1, 3, 5, 6, 4, 2, 8, 1];
     expect(algoSorter.bubble(exampleArr)).toEqual(exampleArr.sort());

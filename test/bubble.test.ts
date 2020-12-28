afdca66690b67538281070283b07f3084ff7ae1b
Luke Hatcher <lukehatcher98@gmail.com>
2020-12-27 19:36:44 -0800

    convert tests to typescript

--- test/bubble.test.ts
+++ test/bubble.test.ts
@@ -1,14 +1,14 @@
-const algoSorter = require('../src/index');
+import { bubble } from '../src/index';
 
 describe('Bubble sort', () => {
   it('should numerically order an unordered array', () => {
     const exampleArr = [1, 3, 5, 6, 4, 2, 8, 1];
-    expect(algoSorter.bubble(exampleArr)).toEqual(exampleArr.sort());
+    expect(bubble(exampleArr)).toEqual(exampleArr.sort());
   });
   it('should numerically order an unordered array of objects', () => {
     const exampleArr = [{ id: 3 }, { id: 1 }, { id: 9 }, { id: 8 }];
     const soln = [{ id: 1 }, { id: 3 }, { id: 8 }, { id: 9 }];
-    const bubbled = algoSorter.bubble(exampleArr, 'id');
+    const bubbled = bubble(exampleArr, 'id');
     let match = true;
     for (let i = 0; i < exampleArr.length; i++) {
       if (bubbled[i].id !== soln[i].id) {
@@ -20,13 +20,13 @@ describe('Bubble sort', () => {
   it('should return nothing if array containes string', () => {
     const exampleArr = [1, 3, 5, 'hello'];
     const exampleArr2 = [1, 3, 5, false];
-    expect(algoSorter.bubble(exampleArr)).toBe(undefined);
-    expect(algoSorter.bubble(exampleArr2)).toBe(undefined);
+    expect(bubble(exampleArr)).toBe(undefined);
+    expect(bubble(exampleArr2)).toBe(undefined);
   });
   it('should return nothing if array of objects containes key/val pair with not numerical val', () => {
     const exampleArr = [{ id: 3 }, { id: 'hello' }, { id: 9 }, { id: 8 }];
     const exampleArr2 = [{ id: 3 }, { id: true }, { id: 9 }, { id: 8 }];
-    expect(algoSorter.bubble(exampleArr)).toBe(undefined);
-    expect(algoSorter.bubble(exampleArr2)).toBe(undefined);
+    expect(bubble(exampleArr)).toBe(undefined);
+    expect(bubble(exampleArr2)).toBe(undefined);
   });
 });
--- test/selection.test.ts
+++ test/selection.test.ts
@@ -1,14 +1,14 @@
-const algoSorter = require('../src/index');
+import { selection } from '../src/index';
 
 describe('Selection sort', () => {
   it('should numerically order an unordered array', () => {
     const exampleArr = [1, 3, 5, 6, 4, 2, 8, 1];
-    expect(algoSorter.selection(exampleArr)).toEqual(exampleArr.sort());
+    expect(selection(exampleArr)).toEqual(exampleArr.sort());
   });
   it('should numerically order an unordered array of objects', () => {
     const exampleArr = [{ id: 3 }, { id: 1 }, { id: 9 }, { id: 8 }];
     const soln = [{ id: 1 }, { id: 3 }, { id: 8 }, { id: 9 }];
-    const selected = algoSorter.selection(exampleArr, 'id');
+    const selected = selection(exampleArr, 'id');
     let match = true;
     for (let i = 0; i < exampleArr.length; i++) {
       if (selected[i].id !== soln[i].id) {
@@ -20,13 +20,13 @@ describe('Selection sort', () => {
   it('should return nothing if array containes string', () => {
     const exampleArr = [1, 3, 5, 'hello'];
     const exampleArr2 = [1, 3, 5, false];
-    expect(algoSorter.selection(exampleArr)).toBe(undefined);
-    expect(algoSorter.selection(exampleArr2)).toBe(undefined);
+    expect(selection(exampleArr)).toBe(undefined);
+    expect(selection(exampleArr2)).toBe(undefined);
   });
   it('should return nothing if array of objects containes key/val pair with not numerical val', () => {
     const exampleArr = [{ id: 3 }, { id: 'hello' }, { id: 9 }, { id: 8 }];
     const exampleArr2 = [{ id: 3 }, { id: true }, { id: 9 }, { id: 8 }];
-    expect(algoSorter.selection(exampleArr)).toBe(undefined);
-    expect(algoSorter.selection(exampleArr2)).toBe(undefined);
+    expect(selection(exampleArr)).toBe(undefined);
+    expect(selection(exampleArr2)).toBe(undefined);
   });
 });

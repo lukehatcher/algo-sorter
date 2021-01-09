a26d8a74c14a3a7f2f8f756687f910e6628c8c6b
Luke Hatcher <lukehatcher98@gmail.com>
2021-01-09 03:49:17 -0800

    updated

--- test/merge.test.ts
+++ test/merge.test.ts
@@ -1,32 +1,36 @@
 import { merge } from '../src/index';
 
-describe('Merge sort', () => {
+describe('merge sort', () => {
   it('should numerically order an unordered array', () => {
-    const exampleArr = [1, 3, 5, 6, 4, 2, 8, 1];
+    const exampleArr = [1, 3, 5, 9, 4, 2, 8, 1];
+
+    // console.log(exampleArr.sort());
+    // console.log(merge(exampleArr));
     expect(merge(exampleArr)).toEqual(exampleArr.sort());
+
   });
   it('should numerically order an unordered array of objects', () => {
     const exampleArr = [{ id: 3 }, { id: 1 }, { id: 9 }, { id: 8 }];
     const soln = [{ id: 1 }, { id: 3 }, { id: 8 }, { id: 9 }];
-    const inserted = merge(exampleArr, 'id');
+    const merged = merge(exampleArr, 'id');
     let match = true;
     for (let i = 0; i < exampleArr.length; i++) {
-      if (inserted[i].id !== soln[i].id) {
+      if (merged[i].id !== soln[i].id) {
         match = !match;
       }
     }
     expect(match).toBe(true);
   });
-  it('should return nothing if array containes non number value', () => {
+  it('should throw error if array containes string', () => {
     const exampleArr = [1, 3, 5, 'hello'];
     const exampleArr2 = [1, 3, 5, false];
-    expect(merge(exampleArr)).toBe(undefined);
-    expect(merge(exampleArr2)).toBe(undefined);
+    expect(() => {merge(exampleArr)}).toThrow(Error);
+    expect(() => {merge(exampleArr2)}).toThrow(Error);
   });
-  it('should return nothing if array of objects containes key/val pair with not numerical val', () => {
+  it('should throw error if array of objects containes key/val pair with not numerical val', () => {
     const exampleArr = [{ id: 3 }, { id: 'hello' }, { id: 9 }, { id: 8 }];
-    const exampleArr2 = [{ id: 3 }, { id: false }, { id: 9 }, { id: 8 }];
-    expect(merge(exampleArr, 'id')).toBe(undefined);
-    expect(merge(exampleArr2, 'id')).toBe(undefined);
+    const exampleArr2 = [{ id: 3 }, { id: true }, { id: 9 }, { id: 8 }];
+    expect(() => {merge(exampleArr, 'id')}).toThrow(Error);
+    expect(() => {merge(exampleArr2, 'id')}).toThrow(Error);
   });
 });
